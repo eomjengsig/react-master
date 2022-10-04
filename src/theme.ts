6aff0e3c10a418075b4802edff11b85678ad1479
eomjengsig <travelereom@naver.com>
2022-10-04 15:52:35 +0900

    theme

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+export const lighttheme: DefaultTheme = {
+  bgColor: "white",
+  textColor: "black",
+  btnColor: "tomato",
+};
+export const darktheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "teal",
+};
