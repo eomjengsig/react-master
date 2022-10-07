a767bc74efb7ba25065186176fa806c2efbb671c
eomjengsig <travelereom@naver.com>
2022-10-07 23:09:04 +0900

    before-react-query

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
-import { darktheme, lighttheme } from "./theme";
+import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={darktheme}>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,15 @@
 import { DefaultTheme } from "styled-components";
 
-export const lighttheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#8c7ae6",
 };
+
+/*
 export const darktheme: DefaultTheme = {
   bgColor: "black",
   textColor: "white",
   btnColor: "teal",
 };
+*/
